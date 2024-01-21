c73f4b4ad172795001e23f4b745085bb7a18e27e
David Ortega <davidortegaiglesias@gmail.com>
2024-01-21 21:57:57 +0100

    add transformation list

--- KCDragonBall/TransformationList/Controller/TransformationListTableViewController.swift
+++ KCDragonBall/TransformationList/Controller/TransformationListTableViewController.swift
@@ -11,35 +11,35 @@ final class TransformationListTableViewController: UIViewController {
     typealias DataSource = UITableViewDiffableDataSource<Int, DragonBallHero>
     typealias Snapshot = NSDiffableDataSourceSnapshot<Int, DragonBallHero>
 
-	// MARK: - Outlets
-	@IBOutlet weak var tableView: UITableView!
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
 
-	// MARK: - Model
-	private var transformations: [HeroTransformation] = [] {
-		didSet {
-			DispatchQueue.main.async { [weak self] in
-				guard let self else { return }
+    // MARK: - Model
+    private var transformations: [HeroTransformation] = [] {
+        didSet {
+            DispatchQueue.main.async { [weak self] in
+                guard let self else { return }
 
-				self.tableView.reloadData()
-			}
-		}
-	}
-	
+                self.tableView.reloadData()
+            }
+        }
+    }
+
     private let networkModel = NetworkModel.shared
     private var dataSource: DataSource?
-	
-	init(transformations: [HeroTransformation]) {
-		self.transformations = transformations
-		super.init(nibName: nil, bundle: nil)
-	}
 
-	@available(*, unavailable)
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
+    init(transformations: [HeroTransformation]) {
+        self.transformations = transformations
+        super.init(nibName: nil, bundle: nil)
+    }
 
-	// MARK: - View Lifecycle
-	override func viewDidLoad() {
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
     }
@@ -47,54 +47,57 @@ final class TransformationListTableViewController: UIViewController {
 
 // MARK: - Configuration
 private extension TransformationListTableViewController {
-	func setUpView() {
-		title = "Transformations"
-		tableView.dataSource = self
-		tableView.delegate = self
-		tableView.register(
-			UINib(
-				nibName: TransformationTableViewCell.nibName,
-				bundle: nil), forCellReuseIdentifier: TransformationTableViewCell.identifier)
-		tableView.estimatedRowHeight = 112
+    func setUpView() {
+        title = "Transformations"
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(
+            UINib(
+                nibName: TransformationTableViewCell.nibName,
+                bundle: nil), forCellReuseIdentifier: TransformationTableViewCell.identifier)
+        tableView.estimatedRowHeight = 112
 
-	}
+    }
 }
 
-	// MARK: - Table View Data Source
+// MARK: - Table View Data Source
 extension TransformationListTableViewController: UITableViewDataSource {
-	func numberOfSections(
-		in tableView: UITableView
-	) -> Int {
-		return 1
-	}
+    func numberOfSections(
+        in tableView: UITableView
+    ) -> Int {
+        return 1
+    }
 
-	func tableView(
-		_ tableView: UITableView,
-		numberOfRowsInSection section: Int
-	) -> Int {
-		return transformations.count
-	}
-	func tableView(
-		_ tableView: UITableView,
-		cellForRowAt indexPath: IndexPath
-	) -> UITableViewCell {
-		guard let cell = tableView.dequeueReusableCell(withIdentifier: TransformationTableViewCell.identifier, for: indexPath) as? TransformationTableViewCell else {
-			return UITableViewCell()
-		}
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return transformations.count
+    }
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: TransformationTableViewCell.identifier,
+            for: indexPath
+        ) as? TransformationTableViewCell else {
+            return UITableViewCell()
+        }
 
-		cell.configure(with: transformations[indexPath.row])
+        cell.configure(with: transformations[indexPath.row])
 
-		return cell
+        return cell
 
-//		let cell = UITableViewCell()
-//		// ***************
-//		let dragonBallHero = heroes[indexPath.row]
-//		var content = cell.defaultContentConfiguration()
-//		// ************** coger el texto d heroes
-//		content.text = dragonBallHero.name
-//		cell.contentConfiguration = content
-//		return cell
-	}
+        //		let cell = UITableViewCell()
+        //		// ***************
+        //		let dragonBallHero = heroes[indexPath.row]
+        //		var content = cell.defaultContentConfiguration()
+        //		// ************** coger el texto d heroes
+        //		content.text = dragonBallHero.name
+        //		cell.contentConfiguration = content
+        //		return cell
+    }
 }
 
 // MARK: - TableView Delegate
@@ -103,9 +106,8 @@ extension TransformationListTableViewController: UITableViewDelegate {
         _ tableView: UITableView,
         didSelectRowAt indexPath: IndexPath
     ) {
-//		let transformation = transformations[indexPath.row]
-//
-//		let transformationDetailViewController = TransformationDetailViewController(hero: transformation)
-//		navigationController?.show(transformationDetailViewController, sender: nil)
+        let transformation = transformations[indexPath.row]
+        let transformationDetailViewController = TransformationDetailViewController(transformation: transformation)
+        navigationController?.show(transformationDetailViewController, sender: nil)
     }
 }
--- KCDragonBall/TransformationList/Controller/TransformationTableViewCell.swift
+++ KCDragonBall/TransformationList/Controller/TransformationTableViewCell.swift
@@ -9,31 +9,31 @@ import UIKit
 
 final class TransformationTableViewCell: UITableViewCell {
 
-	static let identifier = "TransformationTableViewCellIdentifier"
-	static let nibName = "TransformationTableViewCell"
+    static let identifier = "TransformationTableViewCellIdentifier"
+    static let nibName = "TransformationTableViewCell"
 
-	// MARK: - Outlets
-	@IBOutlet weak var heroDescription: UILabel!
-	@IBOutlet weak var heroName: UILabel!
-	@IBOutlet weak var chevronImage: UIImageView!
-	@IBOutlet weak var heroImage: UIImageView!
-	
-	// MARK: - Lifecycle
-	override func awakeFromNib() {
-		super.awakeFromNib()
-		self.selectionStyle = .none
-	}
+    // MARK: - Outlets
+    @IBOutlet weak var heroDescription: UILabel!
+    @IBOutlet weak var heroName: UILabel!
+    @IBOutlet weak var chevronImage: UIImageView!
+    @IBOutlet weak var heroImage: UIImageView!
 
-	// MARK: - Configuration
-	func configure(with transformation: HeroTransformation) {
-		heroName.text = transformation.name
-		heroDescription.text = transformation.description
-		heroImage.image = UIImage(named: "placeholder")
+    // MARK: - Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.selectionStyle = .none
+    }
 
-		guard let imageURLString = transformation.photo,
-				let imageURL = URL(string: imageURLString) else {
-			return
-		}
-		heroImage.setImage(url: imageURL)
-	}
+    // MARK: - Configuration
+    func configure(with transformation: HeroTransformation) {
+        heroName.text = transformation.name
+        heroDescription.text = transformation.description
+        heroImage.image = UIImage(named: "placeholder")
+
+        guard let imageURLString = transformation.photo,
+              let imageURL = URL(string: imageURLString) else {
+            return
+        }
+        heroImage.setImage(url: imageURL)
+    }
 }
